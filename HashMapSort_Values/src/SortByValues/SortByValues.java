4977cfb26b2f82b841a067c5c5150ed3d03144ca
dludaya <dludaya.cs@gmail.com>
2014-04-05 02:24:01 -0400

    asc&dsc

--- HashMapSort_Values/src/SortByValues/SortByValues.java
+++ HashMapSort_Values/src/SortByValues/SortByValues.java
@@ -6,7 +6,7 @@ import java.io.*;
 
 public class SortByValues {
 	public static boolean A=true;
-	public static boolean D=true;
+	public static boolean D=false;
 	public static void main(String args[]){
 		Map<String, Integer> m=new HashMap<String, Integer>();
 		m.put("d", 8);
@@ -16,8 +16,13 @@ public class SortByValues {
 		m.put("k", 4);
 		System.out.println("Unsorted Map:"+m);
 		System.out.println("After Sorting.......");
-		Map<String, Integer> sortAsc=Sort(m, D);
+		Map<String, Integer> sortAsc=Sort(m, A);
+		System.out.println("Ascending Order");
 		printMap(sortAsc);
+		System.out.println("After Sorting.......");
+		Map<String, Integer> sortDsc=Sort(m, D);
+		System.out.println("Descending Order");
+		printMap(sortDsc);
 	}
 	private static Map<String, Integer> Sort(Map<String, Integer> map, final boolean order){
 		List<Entry<String, Integer>> list=new LinkedList<Entry<String, Integer>>(map.entrySet());
@@ -39,8 +44,12 @@ public class SortByValues {
 		return m;
 	}
 	public static void printMap(Map<String, Integer> map){
+		int count=0;
 		for(Entry<String, Integer> entry:map.entrySet()){
+			if(count<2){
 			System.out.println("Key:"+entry.getKey()+"   Value:"+entry.getValue());
+			count++;
+			}
 		}
 	}
 }
